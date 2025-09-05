55786d157744d3d5ed95caa1bec301dd27856464
mduy23042005 <lmd23042005@gmail.com>
2025-09-06 03:12:32 +0700

    Initial commit with proper gitignore

---
+++ Assets/Scripts/Controller.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+    private float moveSpeed = 4f;
+    private Rigidbody2D rb;
+    private Vector2 movement;
+    private Animator animator;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        movement.x = Input.GetAxis("Horizontal");
+        movement.y = Input.GetAxis("Vertical");
+
+        Debug.Log(movement);
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+        UpdateAnimation();
+    }
+    private void UpdateAnimation()
+    {
+        animator.SetFloat("moveX", movement.x);
+        animator.SetFloat("moveY", movement.y);
+
+        bool isMove = (movement.x != 0 || movement.y != 0);
+        animator.SetBool("isMove", isMove);
+
+        if (isMove)
+        {
+            // Ưu tiên theo hướng
+            if (Mathf.Abs(movement.x) > Mathf.Abs(movement.y))
+            {
+                if (movement.x > 0)
+                    animator.SetInteger("direction", 3); // Right
+                else
+                    animator.SetInteger("direction", 2); // Left
+            }
+            else
+            {
+                if (movement.y > 0)
+                    animator.SetInteger("direction", 1); // Back
+                else
+                    animator.SetInteger("direction", 0); // Front
+            }
+        }
+    }
+}
