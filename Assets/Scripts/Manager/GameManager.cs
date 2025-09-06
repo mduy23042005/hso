d64c6fe6898009f80546a42d180d961339ecee43
mduy23042005 <lmd23042005@gmail.com>
2025-09-06 17:11:49 +0700

    05:11 PM 06/09/2025 Hoan thanh sua loi

--- Assets/Scripts/Manager/GameManager.cs
+++ Assets/Scripts/Manager/GameManager.cs
@@ -1,10 +1,28 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
 
+public interface IUpdatable
+{
+    void OnUpdate();
+}
 public class GameManager : MonoBehaviour
 {
     private static GameManager instance;
-    public static GameManager Instance => instance;
+    public static GameManager Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = FindObjectOfType<GameManager>();
+                if (instance == null)
+                {
+                    Debug.LogError("No GameManager found in scene!");
+                }
+            }
+            return instance;
+        }
+    }
 
     private List<IUpdatable> updatables = new List<IUpdatable>();
 
--- Assets/Scripts/Systems/IUpdatable.cs
+++
@@ -1,6 +0,0 @@
-﻿using UnityEngine;
-
-public interface IUpdatable
-{
-    void OnUpdate();
-}
