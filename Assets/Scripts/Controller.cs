de35bf9b2cd21d64f1a69b352ca0cc3f2da3af17
mduy23042005 <lmd23042005@gmail.com>
2025-09-06 10:58:12 +0700

    11:00 AM 06/09/2025 Hoàn thành phần di chuyển

--- Assets/Scripts/Controller.cs
+++ Assets/Scripts/Controller.cs
@@ -5,12 +5,16 @@ public class Controller : MonoBehaviour
     private float moveSpeed = 4f;
     private Rigidbody2D rb;
     private Vector2 movement;
+    private Vector2 lastMove = new Vector2(0, -1);
     private Animator animator;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
+
+        animator.SetFloat("LastHorizontal", 0);
+        animator.SetFloat("LastVertical", -1);
     }
     // Update is called once per frame
     void Update()
@@ -26,9 +30,17 @@ public class Controller : MonoBehaviour
         if (movement.x != 0 && movement.y != 0)
         {
             if (Mathf.Abs(movement.x) > Mathf.Abs(movement.y))
+            {
                 movement.y = 0;
+            }
             else
+            {
                 movement.x = 0;
+            }
+        }
+        if (movement != Vector2.zero)
+        {
+            lastMove = movement;
         }
         rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
     }
@@ -36,6 +48,7 @@ public class Controller : MonoBehaviour
     {
         animator.SetFloat("Horizontal", movement.x);
         animator.SetFloat("Vertical", movement.y);
+
         if (movement.x != 0 || movement.y != 0)
         {
             animator.SetBool("isMove", true);
@@ -44,5 +57,8 @@ public class Controller : MonoBehaviour
         {
             animator.SetBool("isMove", false);
         }
+
+        animator.SetFloat("LastHorizontal", lastMove.x);
+        animator.SetFloat("LastVertical", lastMove.y);
     }
 }
